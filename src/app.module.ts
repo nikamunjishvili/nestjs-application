d6a250080bf8b7733b169c3b3cb1603b0cdcdb5b
Nika munjisvili <nikamunja31415@gmail.com>
2024-07-08 19:59:19 +0400

    create env file and install config package

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
     MongooseModule.forRoot(
-      'mongodb+srv://nikamunjishvili:nestjs-auth@re-edu.aplqjps.mongodb.net/?retryWrites=true&w=majority&appName=re-edu',
+      `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@nest-autentication.8lz05oa.mongodb.net/?retryWrites=true&w=majority&appName=nest-autentication`,
     ),
     PostModule,
     UserModule,
