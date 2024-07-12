e7d1b2141be8e4c76f06a5d8b9a89b6f97d7a1f5
Nika munjisvili <nikamunja31415@gmail.com>
2024-07-12 20:27:20 +0400

    fix code

--- src/common/post/post.module.ts
+++ src/common/post/post.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { PostService } from './post.service';
 import { PostController } from './post.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Post, PostSchema } from '../../../libs/post-lib/src/schemas/post.schema';
 import { UserModule } from 'src/common/user/user.module';
+import { PostLibService } from 'libs/post-lib';
 
 @Module({
   imports: [
@@ -11,6 +11,6 @@ import { UserModule } from 'src/common/user/user.module';
     UserModule,
   ],
   controllers: [PostController],
-  providers: [PostService],
+  providers: [PostLibService],
 })
 export class PostModule {}
